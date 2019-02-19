aa22e103602d091a12e23667b8438a0468c2c8f1
Anton <anton.mois33v@gmail.com>
2019-02-19 12:29:04 -0500

    Exercise 3 done

--- exercises.py
+++ exercises.py
@@ -17,8 +17,25 @@ class Person:
 
     def __str__(self):
         return "Person: {}\nPerson's Emotions: {}".format(self.name, self.emotions)
+
+    # exercise 3 - adding an instance method
+    def display_emotions(self):
+        for emotion, degree in self.emotions.items():
+            converted_degreee = None
+            if degree == 1:
+                converted_degreee = "low"
+            elif degree == 2:
+                converted_degreee = "medium"
+            elif degree == 3:
+                converted_degreee = "high"
+            print("I am feeling a {} amount of {}.".format(converted_degreee, emotion))
 # create an instance of the Person class
 print()
 print("Exercise 2:")
 anton = Person("Anton", human_emotions)
 print(anton)
+
+# exercise 3 - state the emotions
+print()
+print("Exercise 3:")
+anton.display_emotions()
